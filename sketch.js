a29ac54b972277c07a2d65a90983e00aec352366
victorbr001 <schinemann.victor@escola.pr.gov.br>
2025-05-29 11:23:18 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,5 +3,17 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("#1ec3ea");
+  
+  fill("orange")
+  circle(195,260,100);  
+  
+  fill("green");
+ rect(104,20,190,230);
+  
+  fill("yellow");
+  triangle(300,10,100,10,200,200);
+  
+  fill("blue");
+  circle(200,25,105);
 }
